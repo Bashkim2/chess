dab84690c3a408f2f5b4a1cbde7213b00567407f
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-03-17 01:14:39 +0100

    updated the new chessboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Chessboard from "./components/chessboard";
 import Helloworld from "./components/helloworld";
-// import "./css/App.css";
+import "./css/App.css";
+import "./css/Output.css";
 
 function App() {
   return <Chessboard />;
--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import "../css/App.css";
 import "../css/Output.css";
 
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalsAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+
 export default function Chessboard() {
-  return <div>Hello WOrld CHess</div>;
+  return <div className="bg-blue-800">Hello WOrld CHess</div>;
 }
