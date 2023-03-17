dc7748b1c58de7a3b1df060be40ddff40b9799f1
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-03-17 01:32:33 +0100

    updated the new chessboard component

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -6,5 +6,9 @@ const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalsAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function Chessboard() {
-  return <div className="bg-blue-800">Hello WOrld CHess</div>;
+  return (
+    <div className="bg-blue-800 w-96 h-96" id="chessboard">
+      Chessboard
+    </div>
+  );
 }
