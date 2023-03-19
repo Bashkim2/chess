a9c0485daa552f2a010c3aea48eaa7028731346e
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-03-20 00:05:38 +0100

    done with the chessboard

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -2,28 +2,28 @@ import React from "react";
 import "../css/App.css";
 import "../css/Output.css";
 
-const verrticalsAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalsAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const horizontalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 export default function Chessboard() {
   let board = [];
 
-  for (let i = verrticalsAxis.length - 1; i >= 0; i--) {
+  for (let i = verticalsAxis.length - 1; i >= 0; i--) {
     for (let j = 0; j < horizontalAxis.length; j++) {
       const number = i + j + 2;
 
       if (number % 2 === 0) {
         board.push(
-          <div className="h-24 w-24">
-            [{horizontalAxis[j]}
-            {verrticalsAxis[i]}]
+          <div className="h-24 w-24 bg-yellow-600">
+            [{horizontalAxis[i]}
+            {verticalsAxis[j]}]
           </div>
         );
       } else {
         board.push(
-          <div className="h-24 w-24 bg-lime-600">
-            [{horizontalAxis[j]}
-            {verrticalsAxis[i]}]
+          <div className="h-24 w-24 bg-white">
+            [{horizontalAxis[i]}
+            {verticalsAxis[j]}]
           </div>
         );
       }
