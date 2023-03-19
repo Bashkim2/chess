6fd3aa6a96072691d2a8b7a574daca9ca9de7589
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-03-19 23:43:40 +0100

    change the size of the board

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -6,14 +6,9 @@ const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const verticalsAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function Chessboard() {
-  // return (
-  //   <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  //     <div>
-  //       <div className="text-xl font-medium text-black">ChitChat</div>
-  //       <p className="text-slate-500">You have a new message!</p>
-  //     </div>
-  //   </div>
-  // );
-
-  return <div className="bg-lime-600 w-96 h-96">Chessboard</div>;
+  return (
+    <div id="chessboard" className="bg-lime-600 w-96 h-96">
+      Chessboard
+    </div>
+  );
 }
