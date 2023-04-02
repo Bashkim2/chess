b6e811941bd5f0b8636894a85f323e756ce32d97
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-04-03 00:19:13 +0200

    Fixed even the black movements

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -3,6 +3,7 @@ import { PieceType, TeamType, Piece } from "../components/chessboard";
 export default class Referee {
   // Function isOccupied
   tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+    console.log("checking");
     const piece = boardState.find((p) => p.x === x && p.y === y);
 
     if (piece) {
@@ -26,26 +27,44 @@ export default class Referee {
     if (type === PieceType.PAWN) {
       if (team === TeamType.OUR) {
         if (py === 1) {
-          if (px === x && (y - py === 1 || y - py === 2)) {
+          if (px === x && y - py === 1) {
             if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
+          } else if (px === x && y - py === 2) {
+            if (
+              !this.tileIsOccupied(x, y, boardState) &&
+              !this.tileIsOccupied(x, y - 1, boardState)
+            ) {
               return true;
             }
           }
         } else {
           if (px === x && y - py === 1) {
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
           }
         }
       } else {
         if (py === 6) {
-          if (px === x && (y - py === -1 || y - py === -2)) {
+          if (px === x && y - py === -1) {
             if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
+          } else if (px === x && y - py === -2) {
+            if (
+              !this.tileIsOccupied(x, y, boardState) &&
+              !this.tileIsOccupied(x, y + 1, boardState)
+            ) {
               return true;
             }
           }
         } else {
           if (px === x && y - py === -1) {
-            return true;
+            if (!this.tileIsOccupied(x, y, boardState)) {
+              return true;
+            }
           }
         }
       }
