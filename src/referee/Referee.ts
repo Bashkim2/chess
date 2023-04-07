c2ca8c0f4fb1db67d81818dab742a90ad80a5522
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-04-08 00:03:51 +0200

    strike implementation

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,10 +1,27 @@
 import { PieceType, TeamType, Piece } from "../components/chessboard";
 
 export default class Referee {
-  // Function isOccupied
+  // Function tileIsOccupied used for if a tile is occupied by another piece
   tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-    console.log("checking");
     const piece = boardState.find((p) => p.x === x && p.y === y);
+
+    if (piece) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Function to show if a tile is occupied by enemy
+  tileIsOccupiedByOpponent(
+    x: number,
+    y: number,
+    boardState: Piece[],
+    team: TeamType
+  ): boolean {
+    const piece = boardState.find(
+      (p) => p.x === x && p.y === y && p.team !== team
+    );
 
     if (piece) {
       return true;
@@ -29,6 +46,7 @@ export default class Referee {
       const specialRow = team === TeamType.OUR ? 1 : 6;
       const pawnDirection = team === TeamType.OUR ? 1 : -1;
 
+      // Movement LOGIC
       if (px === x && py === specialRow && y - py === 2 * pawnDirection) {
         if (
           !this.tileIsOccupied(x, y, boardState) &&
@@ -39,6 +57,21 @@ export default class Referee {
       } else if (px === x && y - py === 1 * pawnDirection) {
         if (!this.tileIsOccupied(x, y, boardState)) {
           return true;
+        }
+      }
+      // Attack Logic
+      else if (x - px === -1 && y - py === pawnDirection) {
+        // Attack in Upper or Bottom left corner
+        if (this.tileIsOccupiedByOpponent(x, y, boardState, team)) {
+          console.log("We can strike!");
+          // return true;
+        }
+      } else if (x - px === 1 && y - py === pawnDirection) {
+        // Attack in Upper or Bottom right corner
+        if (this.tileIsOccupiedByOpponent(x, y, boardState, team)) {
+          console.log("We can strike!");
+
+          // return true;
         }
       }
       return false;
