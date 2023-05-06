b229621b7fa8324f081f2d3c14ce525307124d98
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-06 23:46:03 +0200

    added som comment

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,2 +1,34 @@
+
+/**
+ * Constants
+ */
 export const verticalsAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 export const horizontalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+
+/**
+ * Enum and interfaces
+ */
+
+export interface Piece {
+    image: string;
+    x: number;
+    y: number;
+    type: PieceType;
+    team: TeamType;
+    enPassant?: boolean;
+  }
+  
+  export enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    QUEEN,
+    KING,
+  }
+  
+  export enum TeamType {
+    OUR,
+    OPPONENT,
+  }
