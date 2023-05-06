9cdbc1c41b5a6eb486579526d0fd4f9fe18b71e5
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-06 23:56:03 +0200

    added the rock reference

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,34 +1,49 @@
-
 /**
  * Constants
  */
 export const verticalsAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 export const horizontalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-
 /**
  * Enum and interfaces
  */
 
 export interface Piece {
-    image: string;
-    x: number;
-    y: number;
-    type: PieceType;
-    team: TeamType;
-    enPassant?: boolean;
-  }
-  
-  export enum PieceType {
-    PAWN,
-    BISHOP,
-    KNIGHT,
-    ROOK,
-    QUEEN,
-    KING,
-  }
-  
-  export enum TeamType {
-    OUR,
-    OPPONENT,
-  }
+  image: string;
+  x: number;
+  y: number;
+  type: PieceType;
+  team: TeamType;
+  enPassant?: boolean;
+}
+
+export enum PieceType {
+  PAWN,
+  BISHOP,
+  KNIGHT,
+  ROOK,
+  QUEEN,
+  KING,
+}
+
+export enum TeamType {
+  OUR,
+  OPPONENT,
+}
+
+const initialBoardState: Piece[] = [
+  {
+    image: `public/W_rock.png`,
+    x: 1,
+    y: 7,
+    type: PieceType.ROOK,
+    team: TeamType.OUR,
+  },
+  {
+    image: `public/B_rock.png`,
+    x: 6,
+    y: 0,
+    type: PieceType.ROOK,
+    team: TeamType.OUR,
+  },
+];
