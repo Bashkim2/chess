060cef6eb6ecd5d69562725be5247de06a46bf96
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-05-09 23:43:24 +0200

    comments

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,8 +1,12 @@
+/******************************CONSTANTS***********************************************/
+
 /**
  * Constants
  */
 export const verticalsAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 export const horizontalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+
 
 /**
  * Enum and interfaces
