f4599d7beb2130583d7533bf84d655b732c15844
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-06-18 14:42:30 +0200

    small change

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -177,7 +177,7 @@ export default function Chessboard() {
   for (let i = verticalsAxis.length - 1; i >= 0; i--) {
     for (let j = 0; j < horizontalAxis.length; j++) {
       const number = i + j + 2;
-      let image;
+      let image = "";
 
       pieces.forEach((p) => {
         if (p.x === j && p.y === i) {
