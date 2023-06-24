fca7fcacc527173441e3388b2a967b750a940387
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-06-24 23:56:57 +0200

    unused import

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -3,24 +3,26 @@ import "../../css/Popup.css";
 import React from "react";
 import { closePopup } from "../../reducer/actions/popup";
 import { Status } from "../../constant";
-import PromotionBox from "./PromotionBox/Promotions";
 
-const Popup = ({children}) => {
-
-  const { appState : {status}, dispatch } = useAppContext();
+const Popup = ({ children }) => {
+  const {
+    appState: { status },
+    dispatch,
+  } = useAppContext();
 
   const onClosePopup = () => {
-      dispatch(closePopup())
-  }
+    dispatch(closePopup());
+  };
 
-  if (status === Status.ongoing)
-      return null
+  if (status === Status.ongoing) return null;
 
-  return <div className="popup">
-            {React.Children
-            .toArray(children)
-            .map (child => React.cloneElement(child, { onClosePopup }))}
-  </div>
-}
+  return (
+    <div className="popup">
+      {React.Children.toArray(children).map((child) =>
+        React.cloneElement(child, { onClosePopup })
+      )}
+    </div>
+  );
+};
 
-export default Popup
+export default Popup;
