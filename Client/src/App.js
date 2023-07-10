3449c6601a6a63d9f5bfb158444874e9a7122844
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2023-07-10 19:53:59 +0200

    updated readme

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./css/App.css";
 import Board from "./components/Board/Board";
 import AppContext from "./contexts/Context";
@@ -7,23 +7,35 @@ import { initGameState } from "./constant";
 import Control from "./components/Control/Control";
 import MovesList from "./components/Control/bits/MovesList";
 import TakeBack from "./components/Control/bits/TakeBack";
+import io from "socket.io-client";
+import ConnectionHandler from "./Connection/connectionHandler";
+
+export const socket = io.connect("http://localhost:3001");
 
 export default function App() {
   const [appState, dispatch] = useReducer(reducer, initGameState);
+  const [show, setShow] = useState(true);
 
   const providerState = {
     appState,
     dispatch,
   };
+
   return (
-    <AppContext.Provider value={providerState}>
-      <div className="App">
-        <Board />
-        <Control>
-          <MovesList />
-          <TakeBack />
-        </Control>
-      </div>
-    </AppContext.Provider>
+    <div>
+      {show ? (
+        <ConnectionHandler />
+      ) : (
+        <AppContext.Provider value={providerState}>
+          <div className="App">
+            <Board />
+            <Control>
+              <MovesList />
+              <TakeBack />
+            </Control>
+          </div>
+        </AppContext.Provider>
+      )}
+    </div>
   );
 }
